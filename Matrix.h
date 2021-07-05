37fdd60da054f26d06041a75fdc4d0e08d8ef6b9
Daniel <daaaniel02@gmail.com>
2021-07-04 23:02:38 -0700

    sepearted classes

--- Matrix.h
+++ Matrix.h
@@ -9,15 +9,12 @@
 #include <mutex>
 #include <atomic>
 #include <iostream>
-/* Author: appiad
-	testing
-	Code Sample - Matrix Multiplication and Transposition 
-Build Instuctions: g++ .cpp -std="c++11" -pthread
+#include "Job.h"
+#include "JobQueue.h"
+#include "ThreadPool.h"
 
-	Everything is done in a single .h file except for the 
-	testing done in the main file. Each class will have their own 
-	header and cpp file in a future revision.
-	
+/* 
+Build Instuctions: g++ main_matrix.cpp -std="c++11" -pthread
 
 	Note: JobQueue and ThreadPool are modified versions of Anthony William's
 	ThreadSafeQueue and ThreadPool in his book C++ Concurrency in Action
@@ -26,216 +23,11 @@ Build Instuctions: g++ .cpp -std="c++11" -pthread
 
 */
 //FORWARD DECLARATIONS
-template <class T> class Job;
-template <class T> class JobQueue;
-template <class T> class ThreadPool;
-template <class T> class Matrix;
 template <class T> void 
 call_transpose_one(Matrix<T>*& obj, Matrix<T>*& result, Matrix<T>*& other, unsigned row_index );
 template <class T> void
 call_mult_one(Matrix<T>*& obj, Matrix<T>*& result, Matrix<T>*& rhs, unsigned row_index);
 
-
-
-
-/*	stores a function of type 
-	std::function<void(Matrix<T>*&,Matrix<T>*&,Matrix<T>*&, unsigned)>
-	and its 4 arguments to be used in a JobQueue 
-*/ 
-template <class T>
-class Job{
-public:
-	
-	Job (std::function<void(Matrix<T>*&,Matrix<T>*&,Matrix<T>*&, unsigned)> func, 
-		Matrix<T>* obj_ptr, Matrix<T>* ptr, Matrix<T>* ptr_other, unsigned func_arg2)
-		:
-		m_obj_ptr(obj_ptr),	m_func(func), m_ptr_matrix(ptr), 
-		m_ptr_other_matrix(ptr_other), m_func_arg2(func_arg2)
-
-		{}
-	Matrix<T>* m_obj_ptr; //first arg of m_func
-	std::function<void(Matrix<T>*&,Matrix<T>*&,Matrix<T>*&,unsigned)> m_func;
-	Matrix<T>* m_ptr_matrix; //second arg of m_func
-	Matrix<T>* m_ptr_other_matrix; //third arg of m_func
-	unsigned m_func_arg2;     //fourth arg of m_func
-	
-};
-
-
-/*	Thread-safe queue where objects of type Job will be stored and accessed by
-	a ThreadPool.
-*/
-template <class T>
-class JobQueue{
-public:
-	//Default Constructor
-	JobQueue(){
-		m_data_queue = 	std::queue<Job<T> >();
-	}
-	//Copy Constructor
-	JobQueue(const JobQueue& other): 
-		m_data_queue(other.m_data_queue)
-	{}
-	
-
-	//Attempts to retrieve a Job's function and its arguments from the queue to passed 
-	//parameters. If successful then returns true, else returns false. 
-	bool try_pop(std::function<void(Matrix<T>*&,Matrix<T>*&,Matrix<T>*&, unsigned)>& function, 
-		Matrix<T>*& obj_ptr, Matrix<T>*& ptr, Matrix<T>*& ptr_other,  unsigned& arg){
-
-		std::lock_guard<std::mutex> queue_lck (m_queue_mtx) ;
-		if (m_data_queue.empty()){
-			return false;
-		}
-		//assign the function and its arguments to the passed in parameters
-		Job<T> tmp = m_data_queue.front();
-		function = tmp.m_func;
-		obj_ptr = tmp.m_obj_ptr;
-		ptr = tmp.m_ptr_matrix;
-		ptr_other = tmp.m_ptr_other_matrix;
-		arg = tmp.m_func_arg2;
-		//remove the Job from the queue and return true to indicate success
-		m_data_queue.pop();
-		return true;
-	}
-
-	//Returns true if the queue is empty and false otherwise
-	bool empty() const{
-		std::lock_guard<std::mutex> queue_lck (m_queue_mtx) ;
-		return m_data_queue.empty();
-	}
-	//Returns the size of the queue
-	unsigned size() const{
-		std::lock_guard<std::mutex> queue_lck (m_queue_mtx);
-		return m_data_queue.size();
-	}
-
-	//Pushes Job onto the queue
-	void push(Job<T> j){
-		m_data_queue.push(j);
-	}
-
-private:
-	std::queue<Job<T> > m_data_queue; //queue of jobs
-	mutable std::mutex m_queue_mtx; 
-
-};
-
-/*	Creates threads to do the work stored in the JobQueue object 
-	it is constructed with. Returns control to calling thread once all
-	Jobs are completed. Calling thread is required to call
-	join_all after being notified to awaken.
-
-*/
-template <class T>
-class ThreadPool{
-
-public:
-	ThreadPool(JobQueue<T>& jq, std::mutex& mtx, 
-		std::condition_variable& work_done_flag):
-
-		m_job_queue(jq), m_calling_thread_mtx(mtx),  
-		m_work_done_cv(work_done_flag), m_done(false), m_all_jobs_loaded(false),
-		m_num_jobs_completed(0)
-
-		{	
-			m_num_jobs_assigned = m_job_queue.size();
-			//create as many threads as this machine is capable of running concurrently
-			const unsigned m_thread_count = std::thread::hardware_concurrency();
-			try{
-				for (unsigned i=0;i<m_thread_count;++i){
-					m_threads.push_back(
-						std::thread(&ThreadPool::worker_thread,this));	
-				}
-				assert(m_threads.size() == 4);
-				m_all_jobs_loaded = true;
-			}
-			catch(...){
-				std::cerr << "A problem occured in trying to create the ThreadPool"
-					<< std::endl;
-				throw;
-			}
-		}
-
-
-	/*	Passed to wait function of main thread's condition variable to confirm 
-		that it only wakes up when all threads in the pool are done working and 
-		have been joined. This is to prevent a spurious wake-up. 
-	*/ 
-	bool isDone()  {return unsigned(m_done);}
-
-	//Joins all joinable threads in the pool.
-	void join_all(){
-		assert(m_threads.size() == 4);
-		for(unsigned i=0;i<m_threads.size();++i){
-			if (m_threads[i].joinable() ){
-				m_threads[i].join();
-			}
-		}
-	}
-
-	/* Each thread runs this function and continuously takes Jobs from 
-	   the job queue and completes them until there are none left. At 
-	   that point a single thread will wait for all other threads to return
-	   from this function. That thread will than call notify_calling_thread()
-	   before returning.
-	*/ 
-	void worker_thread(){
-		while (!m_done){
-			//only start once all jobs have been loaded into the queue
-			if (m_all_jobs_loaded){
-				std::function<void(Matrix<T>*&,Matrix<T>*&,Matrix<T>*&, unsigned)> task;
-				Matrix<T>* obj_ptr;
-				Matrix<T>* ptr_matrix;
-				Matrix<T>* ptr_other_matrix;
-				unsigned arg2;
-				if (m_job_queue.try_pop(task, obj_ptr, ptr_matrix, ptr_other_matrix, arg2)){
-					task(obj_ptr, ptr_matrix, ptr_other_matrix, arg2);
-					m_num_jobs_completed += 1;;
-				}
-				//queue is empty therefore all jobs are done
-				else{
-					std::call_once(m_notify_of, 
-						&ThreadPool::notify_calling_thread, this);
-				}
-			}
-			else{
-				std::this_thread::yield();
-			}
-		}
-	}
-
-	//Wakes up the thread that iniated the ThreadPool once ALL Jobs
-	//have been assigned and have been comepletly finished.
-	void notify_calling_thread(){
-		while (1){
-			//wait for any threads in the middle of doing a Job
-			if (m_num_jobs_assigned == unsigned(m_num_jobs_completed)){
-				break;
-			}
-			else{
-				std::this_thread::yield();
-			}
-		}
-		//wake the thread that initiated the ThreadPool.
-		std::unique_lock<std::mutex> calling_thread_lck(m_calling_thread_mtx); 
-		m_done = true;
-		m_work_done_cv.notify_one();
-	}
-
-private:
-	unsigned m_thread_count; //number of threads to create
-	std::atomic<bool> m_done; //notifies all threads in pool all work is done
-	std::vector<std::thread> m_threads;
-	JobQueue<T> m_job_queue;
-	//notifies main thread all work is done so it can wake and continue
-	std::condition_variable& m_work_done_cv;
-	std::once_flag m_notify_of;
-	std::mutex& m_calling_thread_mtx; //mutex of the thread that iniated the pool
-	std::atomic_bool m_all_jobs_loaded; //flag to signal threads may begin work
-	std::atomic<unsigned> m_num_jobs_completed; //number of completed Jobs
-	unsigned m_num_jobs_assigned; //number of Jobs the job_queue starts of with
-};
 
 /*
 	Class capable of representing a Matrix of different types. Provides abiltiy
@@ -297,7 +89,8 @@ template <class T>
 Matrix<T>::Matrix() :
 	m_num_rows(0),
 	m_num_cols (0)
-{}
+{
+}
 
 //Copy Constructor
 template <class T> 
